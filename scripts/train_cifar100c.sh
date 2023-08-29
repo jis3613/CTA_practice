0ca27311fda5805e76ceffec4f0f4f517cd4b6fc
mipal <swpark6230@gmail.com>
2023-08-29 11:20:44 +0900

    script file update

---
+++ scripts/train_cifar100c.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+cd ..
+
+#### Tent ###
+
+CUDA_VISIBLE_DEVICES=1 python cta_eval.py \
+    --data=cifar100 --alg=tent --model=rb_ResNeXt29_32x4d --batch_size=64 --lr=0.0001 \
+    --exp_name lr_0.0002_b64 \
+    --seed 2020
+
+#### EATA ###
+
+CUDA_VISIBLE_DEVICES=1 python cta_eval.py \
+    --data=cifar100 --alg=eata --model=rb_ResNeXt29_32x4d --batch_size=64 --lr=0.00025 \
+    --exp_name lr_0.00025_b64 \
+    --seed 2020
+
+#### Cotta ###
+
+CUDA_VISIBLE_DEVICES=1 python cta_eval.py \
+    --data=cifar100 --alg=cotta --model=rb_ResNeXt29_32x4d --batch_size=64 --lr=0.001 \
+    --exp_name lr_0.001_b64 \
+    --seed 2020
